19842141c41709516221afbecd53dc6ae67dcb4d
svitoos <svitoos@gmail.com>
2019-04-05 16:09:26 +0300

    конфиг

--- src/main/java/svitoos/PersonalChunkloaderOC/Config.java
+++ src/main/java/svitoos/PersonalChunkloaderOC/Config.java
@@ -19,6 +19,11 @@ class Config {
     chunkloaderUpgradeRecipe =
         configuration.getBoolean(
             "chunkloaderUpgrade", "recipe", true, "Register Chunkloader Upgrade Recipe");
+    logRejectedReason = configuration.getBoolean("logRejectedReason", "logging", false, "");
+    chunkloaderLogLevel = configuration.getInt("chunkloaderLogLevel", "logging", 1, 0, 5, "");
+    maxTicketsPerPlayer =
+        configuration.getInt("maxTicketsPerPlayer", "general", 3, 0, Integer.MAX_VALUE, "");
+    tickFrequency = configuration.getInt("tickFrequency", "general", 10, 1, Integer.MAX_VALUE, "");
     configuration.save();
   }
 }
